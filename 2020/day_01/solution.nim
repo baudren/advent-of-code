62cea0d17105928e68842cae4084d8c4c0ec173e
Benjamin Audren <benj_audren@yahoo.fr>
2020-12-01 06:50:55 +0100

    improved perf day 01

--- 2020/day_01/solution.nim
+++ 2020/day_01/solution.nim
@@ -2,20 +2,23 @@ import strutils
 import sequtils
 import sugar
 
+let test = "1721,979,366,299,675,1456".split(",").mapIt(parseInt(it))
 let integers = readFile("data.txt").splitLines().mapIt(parseInt(it))
 
 proc sol1(integers: seq[int]): int =
     for i in integers:
-        for j in integers:
-            if i + j == 2020:
-                return i*j
+        if integers.contains(2020-i):
+            return i*(2020-i)
 
 proc sol2(integers: seq[int]): int =
-    for i in integers:
-        for j in integers:
-            for k in integers:
-                if i + j + k == 2020:
-                    return i*j*k
+    for i in 0..<integers.len():
+        for j in i..<integers.len():
+            if integers[i] + integers[j] < 2020:
+                let dk = 2020 - integers[i] - integers[j]
+                if integers.contains(dk):
+                    return dk * integers[i] * integers[j]
 
+assert sol1(test) == 514579
+assert sol2(test) == 241861950
 dump sol1(integers)
 dump sol2(integers)
