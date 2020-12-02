0691529b628a7a40592c36d5acbe0223b8f029ea
Benjamin Audren <benjamin.audren@elca.ch>
2020-12-02 08:31:42 +0100

    day 2 with regex but still super slow

--- 2020/day_02/solution.nim
+++ 2020/day_02/solution.nim
@@ -1,38 +1,29 @@
 import strutils
 import sugar
-import sequtils
+import re
+
+let regex = re"(\d+)-(\d+) (\D): (\D+).*"
 
 let test = "1-3 a: abcde,1-3 b: cdefg,2-9 c: ccccccccc".split(",")
 let data = readFile("data.txt").splitLines()
 
-# TODO Use regex for parsing, but which module???
+# why are regex so slow??
 proc sol1(data: seq[string]): int =
     for line in data:
-        let
-            policy = line.split(":")[0]
-            password = line.split(":")[1].strip()
-            letter = policy.split(" ")[1]
-            min_n = parseInt(policy.split(" ")[0].split("-")[0])
-            max_n = parseInt(policy.split(" ")[0].split("-")[1])
-            count = password.count(letter)
-        if count >= min_n and count <= max_n:
-            result += 1
+        if line =~ regex:
+            let
+                (min_n, max_n, letter, password) = (parseInt(matches[0]), parseInt(matches[1]), matches[2], matches[3])
+                count = password.count(letter)
+            if count >= min_n and count <= max_n:
+                result += 1
 
 proc sol2(data: seq[string]): int =
     for line in data:
-        let
-            policy = line.split(":")[0]
-            password = line.split(":")[1].strip()
-            letter = policy.split(" ")[1][0]
-            index_1 = parseInt(policy.split(" ")[0].split("-")[0])
-            index_2 = parseInt(policy.split(" ")[0].split("-")[1])
-        var count: int = 0
-        if password[index_1-1] == letter:
-            count = count + 1
-        if password[index_2-1] == letter:
-            count += 1
-        if count == 1:
-            result += 1
+        if line =~ regex:
+            let
+                (index_1, index_2, letter, password) = (parseInt(matches[0]), parseInt(matches[1]), matches[2][0], matches[3])
+            if (password[index_1-1] == letter) != (password[index_2-1] == letter):
+                result += 1
 
 assert sol1(test) == 2
 dump sol1(data)
