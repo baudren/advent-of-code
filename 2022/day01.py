add3a61ab45b0a3801ccb846572df0917a4b488a
Benjamin Audren <benj_audren@yahoo.fr>
2022-12-01 06:35:58 +0100

    2022 day 01

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,26 +1,73 @@
 from rich import print
-from utils import load_string, load_lines
+from utils import load, file_to_lines, file_to_ints
 
 
 def sol1(a):
-    return 1
+    data = file_to_lines(a)
+    cur = 0
+    max_ = 0
+    for e in data:
+        if e.strip():
+            cur += int(e.strip())
+        else:
+            if cur > max_:
+                max_ = cur
+            cur = 0
+    return max_
 
 
 def sol2(a):
-    return 0
+    data = file_to_lines(a)
+    foods = []
+    cur = 0
+    for e in data:
+        if e.strip():
+            cur += int(e.strip())
+        else:
+            foods.append(cur)
+            cur = 0
+    if cur != 0:
+        foods.append(cur)
+    foods.sort()
+    return sum(foods[-3:])
 
 
 asserts_sol1 = {
-        "3": 1
+        """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000""": 24000
         }
 
 asserts_sol2 = {
-        "3": 3
+        """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000""": 45000
         }
 
 if __name__ == "__main__":
-    data = load_lines()
-    print(data)
+    data = load()
     for d,expected in asserts_sol1.items():
         assert sol1(d) == expected, f"'sol1({d})' expected '{expected}' but was '{sol1(d)}'"
     print(f"\n{sol1(data)=}\n")
--- 2022/utils.py
+++ 2022/utils.py
@@ -3,9 +3,11 @@ import __main__
 def get_filename():
     return __main__.__file__.replace(".py", ".txt")
 
-
-def load_lines():
-    return [l.strip() for l in open(get_filename(), 'r').readlines() if l.strip()]
-
-def load_string():
+def load():
     return open(get_filename(), 'r').read().strip()
+
+def file_to_lines(a):
+    return [l.strip() for l in a.split("\n")]
+
+def file_to_ints(a):
+    return [int(l.strip()) for l in a.split("\n") if l.strip()]
